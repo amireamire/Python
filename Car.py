bc270378ad95cd21a4c92a4a218ea85849168a24
amire <amire@gmail.com>
2025-11-02 14:26:22 +0330

    Second Commit By Master@Fedora Date : 1404/08/11  14:26  Create Dictionary And Define Class And Review List

---
+++ Car.py
@@ -0,0 +1,12 @@
+class Car :
+ programmer="amire.amir26@gmail.com"
+
+ def  message(self) :
+  print("Wellcome To My Car")
+
+
+ def carON(self) :
+    print("Car is ON")
+
+ def  carOFF(self) :
+    print("Car is OFF")
---
+++ dictionary.py
@@ -0,0 +1,20 @@
+
+def  defineDictionary() :
+ favo = {
+   'first'  :'GIS'  ,
+   'second' : 'RS'
+ }
+
+ favo['third'] = 'Sport'
+ favo['forth'] ='Networking'
+ 
+
+ language = dict( first  ='c++' , second = 'python')
+ colors=dict(first="Red" ,second="blue", third="green")
+
+
+#display Favo 
+ print(favo)
+ print(language)
+ print(colors)
+ print(colors['first'])
